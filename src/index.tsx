c597599aa57d2fc71b2cc409595349327a8c6f0f
[yousincha] <[yousincha@naver.com]>
2023-12-22 15:11:20 +0900

    20231222-2 commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "USE_REDUX",
+});
+console.log("store.getState", store.getState());
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <App
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      />
+    </React.StrictMode>
+  );
+
+render();
+store.subscribe(render);
